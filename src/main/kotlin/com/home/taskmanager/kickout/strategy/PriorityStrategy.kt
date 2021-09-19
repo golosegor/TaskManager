f3d5c72e7adffee34df91e843368c2c613d79670
Egor Goloshchapov <Egor_Goloshchapov@epam.com>
2021-09-19 23:49:00 +0200

    init: comparing priority numeric value

--- src/main/kotlin/com/home/taskmanager/kickout/strategy/PriorityStrategy.kt
+++ src/main/kotlin/com/home/taskmanager/kickout/strategy/PriorityStrategy.kt
@@ -8,6 +8,6 @@ class PriorityStrategy : KickOutStrategy {
     companion object : KLogging()
 
     override fun findProcessToKick(existingProcesses: List<Process>, processToAdd: Process): Process? {
-        return existingProcesses.firstOrNull { it.priority < processToAdd.priority }
+        return existingProcesses.firstOrNull { it.priority.value < processToAdd.priority.value }
     }
 }
